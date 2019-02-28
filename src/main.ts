8783e9d6d4220c2293006bdf45d794602da41e9e
iJungleboy <daniel.mettler@2sic.com>
2019-02-28 08:04:33 +0100

    minor

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import chalk from 'chalk';
 import * as moment from 'moment';
 import ndarray = require('ndarray');
-import * as nj from 'numjs';
 import * as mainChristoph from './2cb/main';
 import * as mainDaniel from './2dm/main';
 import * as mainLuca from './2lt/main';
 import { User, user } from './environment';
 import { chart } from './libs/charting';
 import { getConfig } from './libs/config';
-import { readFileSync, writeFileSync } from './libs/file-operations';
 
 console.log(
   chalk.blue(
@@ -21,8 +19,6 @@ console.log(
  */
 
 const config = getConfig('assets/b_small.in');
-
-console.log(chalk.greenBright(config.maxSquares.toString()));
 
 switch (user) {
   case User.DANIEL:
